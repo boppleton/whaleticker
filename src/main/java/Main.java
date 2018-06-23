714d4b7c898ea488f90afd4adb0793857fa0e8ca
boppleton <bots@boppleton.com>
2018-06-22 20:44:34 -0600

    remove BroadcastListener class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,56 +1,36 @@
 import gui.LaunchWindow;
 import org.pushingpixels.substance.api.skin.*;
-import websocket.BroadcastListener;
-import websocket.Broadcaster;
-import websocket.exchange.binance.BinanceClient;
-import websocket.exchange.bitfinex.BitfinexClient;
-import websocket.exchange.bitmex.BitmexClient;
-import websocket.exchange.gdax.GdaxClient;
-import websocket.exchange.okex.OkexClient;
-
 import javax.swing.*;
-import java.io.IOException;
-import java.net.URISyntaxException;
 
 public class Main {
 
-
-
-    public static void main(String[] args) throws URISyntaxException, InterruptedException {
+    public static void main(String[] args) {
 
         startLaunchWindow();
-
-        BroadcastListener broadcastListener = new BroadcastListener();
-
-
-
-
-
     }
 
     private static void startLaunchWindow() {
 
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    UIManager.setLookAndFeel(new SubstanceGraphiteAquaLookAndFeel());
-                } catch (Exception e) {
-                    System.out.println("Substance Graphite failed to initialize");
-                }
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(new SubstanceGraphiteAquaLookAndFeel());
+            } catch (Exception e) {
+                System.out.println("Substance look&feel load error!");
+            }
 
-                LaunchWindow launchWindow = null;
-                try {
-                    launchWindow = new LaunchWindow("whaleticker");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+            LaunchWindow launchWindow = null;
+            try {
+                launchWindow = new LaunchWindow("whaleticker");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-                launchWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //set X close
-                launchWindow.setSize(220, 300); //set dimensions
-                launchWindow.setLocationRelativeTo(null); //null makes it open in the center
-                launchWindow.setVisible(true); //show window
-
-            }});
+            if (launchWindow != null) {
+                launchWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                launchWindow.setSize(220, 300);
+                launchWindow.setLocationRelativeTo(null);
+                launchWindow.setVisible(true);
+            }
+        });
     }
-
 }
--- src/main/java/websocket/Broadcaster.java
+++ src/main/java/websocket/Broadcaster.java
@@ -7,32 +7,27 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Broadcaster implements Serializable {
-    static ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    private static ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private static LinkedList<BroadcastListener> listeners = new LinkedList<>();
 
     public interface BroadcastListener {
         void receiveBroadcast(String message) throws InterruptedException, IOException;
     }
 
-    private static LinkedList<BroadcastListener> listeners = new LinkedList<>();
+    public static synchronized void broadcast(final String message) {
+        for (final BroadcastListener listener : listeners)
+            executorService.execute(() -> {
+                try { listener.receiveBroadcast(message); } catch (Exception e) { e.printStackTrace(); }
+            });
+    }
 
     public static synchronized void register(BroadcastListener listener) {
         listeners.add(listener);
     }
-
     public static synchronized void unregister(BroadcastListener listener) {
         listeners.remove(listener);
     }
 
-    public static synchronized void broadcast(final String message) {
-        for (final BroadcastListener listener : listeners)
-            executorService.execute(() -> {
-                try {
-                    listener.receiveBroadcast(message);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-    }
+
 }
